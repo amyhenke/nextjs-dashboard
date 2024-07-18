e3be1c36d8f20280da6b227f59418024b2948e12
amyhenke <amyleehenke1@gmail.com>
2024-07-18 14:55:56 +0100

    access env variables in deployment

--- auth.ts
+++ auth.ts
@@ -21,14 +21,22 @@ export const { auth, signIn, signOut } = NextAuth({
     providers: [
         Credentials({
             async authorize(credentials) {
-                const parsedCredentials = z.object({ email: z.string().email(), password: z.string().min(6) }).safeParse(credentials)
+                const parsedCredentials = z
+                    .object({
+                        email: z.string().email(),
+                        password: z.string().min(6),
+                    })
+                    .safeParse(credentials)
 
                 if (parsedCredentials.success) {
                     const { email, password } = parsedCredentials.data
                     const user = await getUser(email)
                     if (!user) return null
 
-                    const passwordsMatch = await bcrypt.compare(password, user.password)
+                    const passwordsMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    )
                     if (passwordsMatch) return user
                 }
 
@@ -37,4 +45,5 @@ export const { auth, signIn, signOut } = NextAuth({
             },
         }),
     ],
+    secret: process.env.AUTH_SECRET,
 })
